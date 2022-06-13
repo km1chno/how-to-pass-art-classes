4598213997d6471295a16d9d2823ffd59c85463c
Katzper <katzper.michno@gmail.com>
2022-06-14 00:49:53 +0200

    script for creating res directories hierarchy

---
+++ create_dirs.sh
@@ -0,0 +1,17 @@
+rm -rf res
+mkdir res
+mkdir res/Dataset
+mkdir res/Dataset/Cubism
+mkdir res/Dataset/Post-Impressionism
+mkdir res/Dataset/Northern-Renaissance
+mkdir res/Dataset/Pop-Art
+mkdir res/AugmentedDataset
+mkdir res/AugmentedDataset/Cubism
+mkdir res/AugmentedDataset/Post-Impressionism
+mkdir res/AugmentedDataset/Northern-Renaissance
+mkdir res/AugmentedDataset/Pop-Art
+mkdir res/GreyScaleDataset
+mkdir res/GreyScaleDataset/Cubism
+mkdir res/GreyScaleDataset/Post-Impressionism
+mkdir res/GreyScaleDataset/Northern-Renaissance
+mkdir res/GreyScaleDataset/Pop-Art
